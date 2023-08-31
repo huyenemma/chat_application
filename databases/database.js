4dadfe1f6cba899d8f01d3efadbd22545641c52b
huyenemma <huyendo.emm@gmail.com>
2023-08-31 10:37:42 +0300

    sql connection

--- databases/database.js
+++ databases/database.js
@@ -2,7 +2,7 @@ import { postgres } from "../deps.js";
 
 let sql;
 if (Deno.env.get("db_url")) {
-  sql = postgres(Deno.env.get("DATABASE_URL"));
+  sql = postgres(Deno.env.get("db_url"));
 } else {
   sql = postgres({});
 }
